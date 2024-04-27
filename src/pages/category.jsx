33ef47372f15e7314437891c729f2415ff94be5d
withLevi <zengyin.ssr@gmail.com>
2024-04-27 22:40:23 +0800

    Add asynchronous network request function.

--- src/pages/category.jsx
+++ src/pages/category.jsx
@@ -1,36 +1,26 @@
 import React, { PureComponent } from "react";
 import { connect } from "react-redux";
-import {
-  changeBannersAction,
-  changeRecommendsAction,
-} from "../store/actionCreators";
-import axios from "axios";
+import { fetchHomeMultiDataAction } from "../store/actionCreators";
 
 export class Category extends PureComponent {
   componentDidMount() {
-    axios.get("http://123.207.32.32:8000/home/multidata").then((res) => {
-      const banners = res.data.data.banner.list;
-      const recommends = res.data.data.recommend.list;
-
-      this.props.changeBanners(banners)
-      this.props.changeRecommends(recommends)
-    });
+    this.props.fetchHomeMultidata();
   }
   render() {
     return (
       <div className="category">
-        <h2>Category Page</h2>
+        <h2>Category Page: {this.props.counter}</h2>
       </div>
     );
   }
 }
 
+const mapStateToPros = (state) => ({
+  counter: state.counter,
+});
 const mapDispatchToProps = (dispatch) => ({
-  changeBanners(banners) {
-    dispatch(changeBannersAction(banners));
-  },
-  changeRecommends(recommends) {
-    dispatch(changeRecommendsAction(recommends))
+  fetchHomeMultidata() {
+    dispatch(fetchHomeMultiDataAction());
   },
 });
-export default connect(null, mapDispatchToProps)(Category);
+export default connect(mapStateToPros, mapDispatchToProps)(Category);
--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,10 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import { thunk } from "redux-thunk";
 import reducer from "./reducer";
 
-const store = createStore(reducer);
+// 正常情况下store.dispatch(object)
+// 想要派发函数 store.dispatch(function)
+
+const store = createStore(reducer, applyMiddleware(thunk));
 
 export default store;
